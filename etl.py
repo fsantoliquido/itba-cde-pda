77f442ed12a971ff6e8bae7315388a304d8c89e5
Franco Santoliquido <Franco>
2024-09-22 16:15:09 -0300

    manejo de func apis

--- etl.py
+++ etl.py
@@ -1,30 +1,46 @@
-from googleapiclient.discovery import build
+from utils import initialize_youtube_api, get_videos_from_channel, transform_video_data, group_videos_by_date, CHANNEL_IDS
 from datetime import datetime
-import os
+import pandas as pd
 
-#Importamos credenciales y testeamos con un canal. El método build nos permite conectarnos a la api de Youtube
+"""
+    Tiene tres etapas:
+    iterar por cada channel y traerme la información sobre qué videos se publicaron
+    iterar sobre los videos y traernos las statistics y las metemos en un dataframe
+    subimos el dataframe a redshift
+    
+"""
+# Inicializamos la API
+youtube = initialize_youtube_api()
 
-API_KEY = os.getenv('YOUTUBE_API_KEY')
-CHANNEL_ID = 'UC7mJ2EDXFomeDIRFu5FtEbA'
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+# Establezco la fecha de inicio (últimos N días)
+published_after = datetime(2024, 9, 15).isoformat("T") + "Z"
 
-# Establezco la fecha de inicio. Esto debería mirar los últimos N días 
-published_after = datetime(2024, 9, 7).isoformat("T") + "Z"
+all_videos = []
 
-# Le pegamos a la api con un metodo requests, sólo buscando videos y a partir de la fecha que establecimos. 
-request = youtube.search().list(
-    part='snippet',
-    channelId=CHANNEL_ID,
-    publishedAfter=published_after,
-    order='date',  
-    type='video'
-    # ,maxResults=10  #
-)
+# Para cada channel de youtube itero y me traigo los videos
+for channel_id in CHANNEL_IDS:
+    videos = get_videos_from_channel(youtube, channel_id, published_after)
+    transformed_videos = transform_video_data(videos)
+    all_videos.extend(transformed_videos)
 
-response = request.execute()
+# Agrupo por fecha
+grouped_videos = group_videos_by_date(all_videos)
 
-# Printeamos la response
-for item in response['items']:
-    title = item['snippet']['title']
-    published_at = item['snippet']['publishedAt']
-    print(f"Título: {title}, Publicado en: {published_at}")
+# Convertir los datos agrupados en una lista de diccionarios para DataFrame
+video_data = []
+for date, videos in grouped_videos.items():
+    for video in videos:
+        video_data.append({
+            'date': date,
+            'title': video['title'],
+            'published_at': video['published_at']
+        })
+
+# Guardamos los datos en un DataFrame
+df = pd.DataFrame(video_data)
+
+# Guardar en un archivo CSV
+df.to_csv('youtube_videos.csv', index=False)
+
+# Mostrar los primeros registros del DataFrame para verificar
+print(df.head())
